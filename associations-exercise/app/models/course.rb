124101d5b87f710e5081aec9ba67a85fe805dfd9
App Academy Student <student@appacademy.io>
2017-08-16 13:30:50 -0700

    Associateation Exercise Complete

--- associations-exercise/app/models/course.rb
+++ associations-exercise/app/models/course.rb
@@ -1,2 +1,22 @@
 class Course < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+  has_one :prerequisite,
+    primary_key: :prereq_id,
+    foreign_key: :id,
+    class_name: :Course
+
+  has_one :instructor,
+    primary_key: :instructor_id,
+    foreign_key: :id,
+    class_name: :User
+
 end
